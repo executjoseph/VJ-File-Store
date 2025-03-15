2ece4fc6082b3a54da2fc5831e3326b17cb11e53
Joseph <josephmillerhaj@gmail.com>
2025-03-16 01:36:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://vascudigama:address64fg@cluster0.hu6a2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", " ")
 DB_NAME = environ.get("DB_NAME", "Cluster0")
 
 # Auto Delete Information
